6ff0d6679c1b1c035976eed8264e02f6f685cf56
Ken Snyder <ken@ken.net>
2015-01-01 21:33:40 +0000

    real mixin files added this time

--- app/mixins/input-status.js
+++ app/mixins/input-status.js
@@ -1,7 +1,23 @@
 import Ember from 'ember';
 var typeOf = Ember.typeOf;
-var isEmpty = Ember.isEmpty;
 
-import InputStatusMixin from 'ui-mixin-helpers/mixins/input-status';
-
-export default InputStatusMixin;
+export default Ember.Mixin.create({
+	status: 'default',
+	statusIsDefault: Ember.computed.equal('status','default'),
+	statusClass: function() {
+		var status = this.get('status');
+		if (typeOf(status) === 'function') {
+			status = status(this); // resolve function call
+		}
+		if(['error','err'].contains(status)) {
+			return 'status-error';
+		} else if(['warn','warning'].contains(status)) {
+			return 'status-warning';
+		} else if(['success','ok'].contains(status)) {
+			return 'status-success';
+		} else {
+			return 'status-default';
+		}
+	}.property('status','value'), // we look at 'value' for function based statuses
+	statusVisualize: false
+});
